fee927f012036619590fab45044850f354c5d372
Daniel da Silva <daniel@meltingwax.net>
2011-04-04 13:42:29 +0000

    Updated activities
    git-svn-id: https://scheme-droid.googlecode.com/svn/trunk@6 998883aa-690a-9f4b-ff25-be1cd729b007

--- src/net/meltingwax/schemedroid/SchemeDroid.java
+++ src/net/meltingwax/schemedroid/SchemeDroid.java
@@ -1,167 +1,66 @@
 package net.meltingwax.schemedroid;
 
+import java.util.HashMap;
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import jscheme.JScheme;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 /**
- * An Android App Wrapper to JScheme.
+ * The main Scheme Droid home screen activity.
  * 
  * @author daniel@meltingwax.net (Daniel da Silva)
  */
 public class SchemeDroid extends Activity {
-	private static String VERSION = "0.2";
-	private static int EVAL_BUTTON_WIDTH = 60;
+    private WebView mWebView;
+	private String homeHtmlPath = "file:///android_asset/home.html";
 	
-	private JScheme js;
-	private TextView console;
-	private EditText entry;
+	private class HomeWebView extends WebViewClient {		
+		@Override
+		@SuppressWarnings("unchecked")
+	    public boolean shouldOverrideUrlLoading(WebView view, String url) {	    	
+			HashMap<String, Class> map = new HashMap<String, Class>();
+			map.put("schemedroid://about", SchemeREPL.class);
+			map.put("schemedroid://help", SchemeREPL.class);
+			map.put("schemedroid://new", SchemeREPL.class);
+			map.put("schemedroid://open", SchemeREPL.class);			
+			map.put("schemedroid://repl", SchemeREPL.class);
+			
+			if (map.containsKey(url)) {
+                Intent myIntent = new Intent(view.getContext(), map.get(url));
+                startActivityForResult(myIntent, 0);
+			}
+	    	
+	        return true; // TODO: see what should be returned
+	    }
+	}
 	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
+    	mWebView = new WebView(this);
+    	setContentView(mWebView);
+        mWebView.setWebViewClient(new HomeWebView());
         
-        /*
-         * Initialize JScheme 
-         */
-        js = new JScheme();
-        
-        /*
-         * Initialize the View 
-         */
-        Display display = getWindowManager().getDefaultDisplay();
-        
-        ScrollView parentScroller = new ScrollView(this);
-        
-        LinearLayout parentLayout = new LinearLayout(this);
-        parentLayout.setOrientation(LinearLayout.VERTICAL);
-        parentLayout.setVerticalScrollBarEnabled(true);
-                
-        console = new TextView(this);
-        parentLayout.addView(console);
-        
-        // The bottom panel that holds the input entry and eval button
-        LinearLayout bottomPanel = new LinearLayout(this);
-        bottomPanel.setOrientation(LinearLayout.HORIZONTAL);
-        
-        entry = new EditText(this);
-        entry.setWidth(display.getWidth() - EVAL_BUTTON_WIDTH);
-        entry.setOnKeyListener(new View.OnKeyListener() {
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if (keyCode == KeyEvent.KEYCODE_ENTER) {
-					processEntry();
-					return true;
-				}
-				
-				/** Begin parenthesis matching **/
-				/*
-				String currentText = entry.getText().toString();
-				final int n = entry.getSelectionStart();
-				
-				if ( n > 0 && currentText.charAt(n - 1) == ')') {
-					int k = -1;
-					for (int i = n - 2; i >= 0; i++) {
-						if (currentText.charAt(i) == '(') {
-							k = i;
-							break;
-						}
-					}
-					
-					if (k != -1) {
-					}
-				}
-				*/
-				/** end parenthesis matching **/				
-				
-				return false;
-			}
-		});
-        
-        bottomPanel.addView(entry);
-        
-        Button evalButton = new Button(this);
-        evalButton.setWidth(EVAL_BUTTON_WIDTH);
-        evalButton.setText("Eval");
-        evalButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				processEntry();
-			}
-		});
-        bottomPanel.addView(evalButton);
-        
-        parentLayout.addView(bottomPanel);
- 
-        parentScroller.addView(parentLayout);
-        setContentView(parentScroller);
+    	if (savedInstanceState != null) {
+    		mWebView.restoreState(savedInstanceState);
+    	} else {
+    		mWebView.loadUrl(homeHtmlPath);
+    	}
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	super.onCreateOptionsMenu(menu);    	
-    	menu.add("Reset");    	
-    	menu.add("About");
+    	// menu.add("Reset");
     	return true;
     }
     
     public boolean onOptionsItemSelected(MenuItem item) {
-    	// I am pretty sure item.getTile().equal(x) is not the proper way
-    	// to do this, but there have been worse sins committed.
-    	if (item.getTitle().equals("Reset")) {
-    		js = new JScheme();
-    		console.setText("");
-    		entry.setText("");
-    	}
-    	else if (item.getTitle().equals("About")) {
-    		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-    		dialogBuilder.setMessage("Scheme Droid v" + VERSION + "\n"
-    				+ "\n"
-    				+ "Assembled by Daniel da Silva (meltingwax). However "
-    				+ "real credit for the interpretter goes to the JScheme "
-    				+ "authors.\n"
-    				+ "\n"
-    				+ "Send comments and feedback to daniel@meltingwax.net");
-    		dialogBuilder.setCancelable(true);
-    		dialogBuilder.create().show();
-    	}
-    	
     	return true;
-    }
-    
-    /**
-     * Processes the code in the entry EditText and updates the view. 
-     */
-    public void processEntry() {
-    	String code = entry.getText().toString();
-    	
-    	if (! code.trim().equals("")) {
-    		String resp;
-    		
-    		try {
-    			resp = js.eval(code).toString();
-    		} catch (jscheme.SchemeException e) {
-    			resp = e.getMessage();
-    		} catch (jsint.BacktraceException e) {
-    			resp = e.getMessage();
-    		} catch (Exception e) {
-    			resp = "Generic Error: " + e.toString();
-    		}
-    		
-    		console.append("\n>" + code + "\n" + resp);
-    		
-    		entry.setText("");
-    	}
     }
 }
