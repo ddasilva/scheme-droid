e370490c916f381c19910c5dde8541747cbd6b4d
Daniel da Silva <daniel@meltingwax.net>
2014-05-30 05:29:00 -0400

    Setting up testing, adding simple test

---
+++ src/net/meltingwax/schemedroid/test/SchemeDroidTest.java
@@ -0,0 +1,45 @@
+package net.meltingwax.schemedroid.test;
+
+import net.meltingwax.schemedroid.SchemeDroid;
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SchemeDroidTest extends
+		ActivityInstrumentationTestCase2<SchemeDroid> {
+
+	private Activity activity;
+	private TextView console;
+	private EditText entry;
+	private Button evalButton;
+	
+	public SchemeDroidTest() {
+		super(SchemeDroid.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		setActivityInitialTouchMode(false);
+
+		activity = getActivity();
+		
+		console = (TextView) activity.findViewById(
+				net.meltingwax.schemedroid.R.id.console);
+		
+		entry = (EditText) activity.findViewById(
+				net.meltingwax.schemedroid.R.id.code_input);
+
+		evalButton = (Button) activity.findViewById(
+				net.meltingwax.schemedroid.R.id.button_eval);
+	}
+	
+	@UiThreadTest
+	public void testErronousInput() {
+		entry.requestFocus();
+		entry.setText("(a");
+		evalButton.callOnClick();
+	}
+}
