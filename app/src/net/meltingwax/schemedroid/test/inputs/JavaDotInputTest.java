f973d2da62b2824550ae86045eb31a254fbd0e35
Daniel da Silva <daniel@meltingwax.net>
2014-05-31 23:27:39 -0400

    Adding LinkedList tests to JavaDotInputTest

--- app/src/net/meltingwax/schemedroid/test/inputs/JavaDotInputTest.java
+++ app/src/net/meltingwax/schemedroid/test/inputs/JavaDotInputTest.java
@@ -15,4 +15,34 @@ public class JavaDotInputTest extends InputSetTestCase {
 		sendInput("(Math.tan 1.0)");
 		assertConsoleContains("1.55");
 	}
+
+	@UiThreadTest
+	public void testImportLinkedList() {
+		sendInput("(import \"java.util.LinkedList\")");
+		assertConsoleContains("#t");
+	}
+
+	@UiThreadTest
+	public void testConstructLinkedList() {
+		sendInput(
+				"(import \"java.util.LinkedList\")",
+				"(LinkedList.)");
+		assertConsoleContains("[]");
+	}
+
+	@UiThreadTest
+	public void testAddToLinkedList() {
+		sendInput(
+				"(import \"java.util.LinkedList\")",
+				"(define element \"foobar\")",
+				"(define my-ll (LinkedList.))",
+				"(.add my-ll element)",
+				"(define ll-size (.size my-ll))",
+				"(display (string-append \"{ll-size:\" ll-size \"}\"))",
+				"(define ll-head (.get my-ll 0))",
+				"(define are-eq (eq? ll-head element))",
+				"(display (string-append \"{are-eq:\" are-eq \"}\"))");
+		assertConsoleContains("{ll-size:1}");
+		assertConsoleContains("{are-eq:#t}");
+	}
 }
